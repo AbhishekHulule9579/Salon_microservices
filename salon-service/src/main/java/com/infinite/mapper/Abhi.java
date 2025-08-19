c69b188453714201117ad9986f5e6eb253d6924b
AbhishekHulule9579 <abhishekhulule.sknsits.it@gmail.com>
2025-08-19 09:30:19 +0530

    Initial commit

---
+++ salon-service/src/main/java/com/infinite/mapper/Abhi.java
@@ -0,0 +1,15 @@
+package com.infinite.mapper;
+
+public class Abhi {
+    System.out.printlnewnrkjan jkv charw
+            wb
+        wb
+    wb
+                rB
+    wrbwrtbwtbwr
+
+    @Override
+    public String toString() {
+        bwr
+    }
+}
